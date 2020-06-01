4f7c9f57df33b707b3450ec717f7c85242240e64
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2020-06-01 14:46:32 +0300

    Update IZ1.pl

--- IZ1.pl
+++ IZ1.pl
@@ -1,2 +1,2 @@
 write(X,Y,Z):- write(X), write(" "), write(Y), write(" "), write(Z), write("\n").
-proverka(X,Y,P):- X<Y+Z, Y<X+Z, Z<X+Y, P1 is P-X-Y .
+proverka(X,Y,P):- Z is P-X-Y, X<Y+Z, Y<X+Z, Z<X+Y.
