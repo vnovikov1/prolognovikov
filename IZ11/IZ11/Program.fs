c568cb151e5bedc24d7e0b6a3ac1b43b2b4be06a
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2020-06-09 18:12:20 +0300

    Create Program.fs

---
+++ IZ11/IZ11/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open System.Drawing
+open System.IO
+open System.Windows.Forms
+
+//создание формы
+let Form1 =new Form(Text = "Индикатор" ,Width = 400, Height = 300)
+
+//создание поля ввода 
+let Edit1 = new TextBox(Width=100, Left=100, Height=25, Text=" ")
+
+
+//привязываем значаеня к форме 
+Form1.Controls.Add(Edit1)
+
+
+
+// Запускаем форму
+do Application.Run(Form1)
