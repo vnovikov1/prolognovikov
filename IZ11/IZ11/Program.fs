08da3b1b0fd91f5510b89349087e94614448e33c
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2020-06-09 18:34:54 +0300

    Update IZ11

--- IZ11/IZ11/Program.fs
+++ IZ11/IZ11/Program.fs
@@ -7,11 +7,23 @@ open System.Windows.Forms
 let Form1 =new Form(Text = "Индикатор" ,Width = 400, Height = 300)
 
 //создание поля ввода 
-let Edit1 = new TextBox(Width=100, Left=100, Height=25, Text=" ")
+let Edit1 = new TextBox(Width=150, Left=115, Height=25, Text="")
+
+let ProgressBar1 = new ProgressBar(Dock = DockStyle.Bottom)
+
+let ScrollBar1 = new TrackBar(Top = 50, Maximum = 100, Width = 400) 
+
+let Change _ = ProgressBar1.Value <- ScrollBar1.Value
+
+let _ = ScrollBar1.ValueChanged.Add(Change)
 
 
 //привязываем значаеня к форме 
 Form1.Controls.Add(Edit1)
+Form1.Controls.Add(ScrollBar1) 
+Form1.Controls.Add(ProgressBar1)
+
+
 
 
 
