9c211b5ecb4e8648a561720721b3e61dd15791ef
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2020-06-10 04:03:03 +0300

    Update IZ2.pl

--- IZ2.pl
+++ IZ2.pl
@@ -5,4 +5,9 @@ r_list(A,N,I,B):-I1 is I+1, read(X), append(B,[X],B1),r_list(A,N,I1,B1).
 w_list([]):-!.
 w_list([H|T]):-write(H),nl,w_list(T).
 
+sredn([],S,K,[H|T]):- Z is S/K, write(Z), write("Среднее арифметическое").
+sredn(H|T),S,K,[H1|T1]):-S1 is S+H, K1 is K+1, sredn(T,S1,K1,[H1|T1]).
+
+spisok:-read(N),r_list(A,N), sredn(A,0,0,A).
+
 
